83dfaac02662c6b07baa3c6a7891bd24dfde3c4d
samfass <98734803+samfass@users.noreply.github.com>
2022-05-19 13:50:03 +0300

    submit

--- _handle_var_replacement.c
+++ _handle_var_replacement.c
@@ -1,4 +1,25 @@
-if (_strcmp(&commands[i][1], "?") == 0)
+#include "shell.h"
+
+/**
+ * handle_var_replacement - Handles dollar vars '$'
+ * @commands: Array of commands
+*/
+void handle_var_replacement(char **commands)
+{
+	int i;
+	char *env_val;
+
+	if (commands[0] == NULL)
+		return;
+
+	/* Scan command args and replace the needed ones using PATH, and $$, $? */
+	for (i = 1; commands[i] != NULL; i++)
+	{
+		if (commands[i][0] != '$')
+			continue;
+		/* Here the arg starts with '$' */
+
+		if (_strcmp(&commands[i][1], "?") == 0)
 		{
 			free(commands[i]);
 			commands[i] = num_to_str(*process_exit_code());
